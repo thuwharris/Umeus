6e1bc198fa607bdf1426c7ebedcf079441026ca1
Huw Harris <thh489@student.bham.ac.uk>
2017-10-31 20:13:41 +0000

    Fixed config.py to use environment variables

--- umeus/config.py
+++ umeus/config.py
@@ -1,20 +1,20 @@
 import os
 
 # ENCRYPTION
-SECRET_KEY = ''#os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 BCRYPT_LEVEL = 12
 BCRYPT_LOG_ROUNDS = 12
 
 # DATABASE FOR LOCAL USE
-basedir = os.path.abspath(os.path.dirname(__file__))
-basedir = os.path.join(basedir, '..')
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db/database.db')
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# basedir = os.path.join(basedir, '..')
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db/database.db')
 
-# SQLALCHEMY_DATABASE_URI = os.environ['DATABASE']
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE']
 
 # RECAPTCHA
-RECAPTCHA_PUBLIC_KEY  = ''#os.environ['RECAPTCHA_PUBLIC']
-RECAPTCHA_PRIVATE_KEY = ''#os.environ['RECAPTCHA_PRIVATE']
+RECAPTCHA_PUBLIC_KEY  = os.environ['RECAPTCHA_PUBLIC']
+RECAPTCHA_PRIVATE_KEY = os.environ['RECAPTCHA_PRIVATE']
 
 # APP CONFIGURATION
 COMMENTS_PER_PAGE = 15
